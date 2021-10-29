d4f8d15f5e995189cb1459a9d74aa86662a6933e
NishakarKT <nishakarkumar0@gmail.com>
2021-10-29 20:00:02 +0530

    login  frontend

--- app/views.py
+++ app/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+	return render(request, "index.html")
+
+def login(request):
+	if request.user.is_authenticated:
+		return redirect('app:index')
+	else:
+		return render(request, 'login/login.html')
+
+def otp(request):
+	if request.user.is_authenticated:
+		return redirect('app:index')
+	else:
+		if request.method == "POST":
+			aadharNum = request.POST.get("aadharNum")
+			return render(request, 'login/otp.html')
+		else:
+			return redirect('app:login')
